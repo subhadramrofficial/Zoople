11aaa5102a642d5a0ff12ca98794de7428787a7a
subhadramrofficial <subhadramr.official@gmail.com>
2026-09-13 13:16:24 +0530

    commit

---
+++ React/Revise/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Food/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Revise/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer () {
+  return (
+    <footer>
+      <p>&copy;Your website name</p>
+    </footer>
+  )
+}
+
+export default Footer
